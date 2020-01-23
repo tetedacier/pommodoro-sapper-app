15d053b87fb32274955a64a3976703e28693ba8d
tetedacier <fabien.jacq@gmail.com>
2020-01-23 11:35:29 +0100

    Add preload route

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,11 @@
+<script context="module">
+import { waitLocale } from 'svelte-i18n'
+// import 'i18n'
+  export async function preload() {
+    // awaits for the loading of the 'en-US' and 'en' dictionaries
+    return waitLocale()
+  }
+</script>
 <script>
 	import Nav from '../components/Nav.svelte';
 
