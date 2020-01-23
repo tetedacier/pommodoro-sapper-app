9a3ff2e0a088ff40737b1664b3315456eea8a8a4
tetedacier <fabien.jacq@gmail.com>
2020-01-23 11:34:07 +0100

    Initialize i18n store

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+import './i18n';
+
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
+		// init,
 		sapper.middleware()
 	)
 	.listen(PORT, err => {
