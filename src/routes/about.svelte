ac23664a2466e5b8878abd1aeef1f33045958291
tetedacier <fabien.jacq@gmail.com>
2020-04-08 09:42:06 +0200

    Add basic transition effect

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,12 @@
 <svelte:head>
 	<title>About</title>
 </svelte:head>
+<script>
+	import TransitionWrapper from '../components/TransitionWrapper.svelte'
 
-<h1>About this site</h1>
+</script>
+<TransitionWrapper>
+	<h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+	<p>This is the 'about' page. There's not much here.</p>
+</TransitionWrapper>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
 <script>
 	import { _ } from 'svelte-i18n'
 	import {getContext, setContext, onDestroy} from 'svelte'
+	import TransitionWrapper from '../components/TransitionWrapper.svelte'
 	import Topic from '../components/topic.svelte'
 	import Selector from '../components/selector.svelte'
 	import {toSeconds} from '../lib/time.js'
@@ -15,19 +16,20 @@
 	})
 
 </script>
+<TransitionWrapper>
+	<Selector bind:topics={topics} />
 
-<Selector bind:topics={topics} />
-
-<h1>{$_('app_title')}</h1>
-{#if ( topics.length > 0 )}
-	total Time: {toSeconds(topics.reduce((acc, topic) => acc + topic.ellapsedTime, 0))} s
-	<ul>	
-		{#each topics as topic}
-			<Topic bind:value={topic} />
-		{/each}
-	</ul>
-{:else}
-	<p>
-		No topics available yet.
-	</p>
-{/if}
+	<h1>{$_('app_title')}</h1>
+	{#if ( topics.length > 0 )}
+		total Time: {toSeconds(topics.reduce((acc, topic) => acc + topic.ellapsedTime, 0))} s
+		<ul>
+			{#each topics as topic}
+				<Topic bind:value={topic} />
+			{/each}
+		</ul>
+	{:else}
+		<p>
+			No topics available yet.
+		</p>
+	{/if}
+</TransitionWrapper>
