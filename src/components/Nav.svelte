d67f21067b25a33345bbf1eda0b817d5eb56d1e6
tetedacier <fabien.jacq@gmail.com>
2020-04-30 11:51:58 +0200

    Move posts to todos

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -59,8 +59,8 @@
 		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
 		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
+		<!-- for the todo link, we're using rel=prefetch so that Sapper prefetches
+		     the todo data when we hover over the link or tap it on a touchscreen -->
+		<li><a rel=prefetch class:selected='{segment === "todo"}' href='todo'>todo</a></li>
 	</ul>
 </nav>
