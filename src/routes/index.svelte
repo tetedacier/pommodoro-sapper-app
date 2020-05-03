324dc5b5318a155946b4d4458be1f3af7341073d
tetedacier <fabien.jacq@gmail.com>
2020-05-03 17:49:13 +0200

    Add more check for the local dailies object

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,41 +17,57 @@
 	function fetchTopics() {
 		const date = new Date()
 		const dateStamp = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}`
-		fetch(`http://localhost:3010/dailies?date=${dateStamp}`)
-			.then(r => r.json(), err => console.error(err))
-			.then(dayly => {
-				console.log(dayly)
-				dailies[dateStamp] = dayly[0].id;
-				topics = dayly[0].activities
-			}, err => console.error(err))
+		if (dailies) {
+			if (dailies[dateStamp]) {
+				console.log('already fetched')
+			} else {
+				fetch(`http://localhost:3010/dailies?date=${dateStamp}`)
+					.then(r => r.json(), err => console.error(err))
+					.then(([dayly]) => {
+						console.log(dayly)
+						if(dayly) {
+							dailies[dateStamp] = dayly;
+							topics = dayly.activities
+						}
+						else {
+							console.error(`no daily available for ${dateStamp}`)
+						}
+					}, err => console.error(err))
+			}
+		} else {
+			console.log('dailies in retrieval')
+		}
 	}
 	function uploadTopics() {
 		const date = new Date()
 		const dateStamp = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}`
-		console.warn(topics)
-		console.warn(JSON.stringify(topics))
-		if (dailies[dateStamp]) {
-			window.fetch(`http://localhost:3010/dailies/${dailies[dateStamp]}`, {
-				method: 'PUT',
-				headers:{
-					'Content-Type': 'application/json; charset=utf-8'
-				},
-				body: JSON.stringify({
-					date: dateStamp,
-					activities: topics
+		if (dailies) {
+			if (dailies[dateStamp]) {
+				window.fetch(`http://localhost:3010/dailies/${dailies[dateStamp].id}`, {
+					method: 'PUT',
+					headers:{
+						'Content-Type': 'application/json; charset=utf-8'
+					},
+					body: JSON.stringify({
+						date: dateStamp,
+						activities: topics
+					})
 				})
-			})
-				.then(r => r.json(), err => console.error(err))
+					.then(r => r.json(), err => console.error(err))
+			}
 		}
 	}
 	const saveTopics = (event) => {
 		uploadTopics()
 	}
-	onDestroy(uploadTopics)
-	onMount(fetchTopics)
+	onDestroy(() => {
+		uploadTopics()
+	})
+	onMount(() => {
+		fetchTopics()
+	})
 </script>
 <TransitionWrapper>
-
 	<h1>{$_('app_title')}</h1>
 	<button type="button" on:click={saveTopics}>save</button>
 	<Selector bind:topics={topics} />
